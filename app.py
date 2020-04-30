c93e0efdda226b29a4a73a128570536c0b683063
archxii <mostheidt@gmail.com>
2020-04-30 11:17:16 -0700

    Added /Tasks/Done route.

--- app.py
+++ app.py
@@ -24,5 +24,17 @@ def get_all_tasks():
     return make_response(jsonify(task_list), 200)
 
 
+@app.route('/Tasks/Done', methods=["GET"])
+def get_done_tasks():
+    task_list = []
+    task1 = Task("Do Dishes", "Clean them", date(2020, 5, 12))
+    task2 = Task("Mow the lawn", "Spinny knife go brrrr", date(2020, 6, 21), complete=True)
+    tasks = [task1, task2]
+    for task in tasks:
+        if task.complete:
+            task_list.append(task.to_dict())
+    return make_response(jsonify(task_list), 200)
+
+
 if __name__ == '__main__':
     app.run()
