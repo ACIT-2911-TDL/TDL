f0b9bf8de9213e34a2677aabafb0575edce940d1
archxii <mostheidt@gmail.com>
2020-04-29 19:45:54 -0700

    Created hardcoded get all tasks API route. Needs deadline parameter.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from task import Task
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('Tasks/All')
+# once the database is running, this will be changed to get the list from there, eliminating the parameter.
+def get_all_tasks():
+    task_list = []
+    task1 = Task("Do Dishes", "Clean them", DEADLINE)
+    task2 = Task("Mow the lawn", "Spinny knife go brrrr", DEADLINE)
+    tasks = [task1, task2]
+    for task in tasks:
+        task_list.append(task.to_dict())
+    return make_response(jsonify(task_list, 400))
 
 
 if __name__ == '__main__':
